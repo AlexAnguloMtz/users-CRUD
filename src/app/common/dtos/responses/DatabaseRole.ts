0bf101328b0d4bd25c9b5c9403aea443b24c29fe
alex <raulalexo100@gmail.com>
2023-09-19 14:00:36 -0700

    Commit before refactoring some SQL queries

--- src/app/common/dtos/responses/DatabaseRole.ts
+++ src/app/common/dtos/responses/DatabaseRole.ts
@@ -9,6 +9,11 @@ export type DatabaseRole = {
     tablesPrivileges: Array<TablePrivileges>
 }
 
+// This function takes a role object and if it has a certain privilege,
+// it returns the exact same role object but with that exact privilege removed.
+// If it does not have that privilege, then it returns the exact same role object but with 
+// that privilege added to it.
+// This function is essentially a toggle for privileges in a role object.
 export function togglePrivilege(role: DatabaseRole, privilege: TablePrivilege): DatabaseRole {
     const privilegeIndex = role.tablesPrivileges.findIndex(
         (tablePrivilege) =>
